4eba10ef95d044d1481795ec16cb76a67be5d3f7
Asanka Sayakkara <asanka.code@gmail.com>
2018-07-04 11:12:39 +0100

    Updated the installation instructions.

--- tensorboard-visualization.py
+++ tensorboard-visualization.py
@@ -8,8 +8,8 @@ Created on Tue Jul  3 16:28:50 2018
 """
 
 # Need the following setting to remove the warning message
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+#import os
+#os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 
 # Importing tensorflow module
 import tensorflow as tf
